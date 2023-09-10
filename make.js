63b1672a5b75d33813e8eb88a2cf508d41f22bfd
Xifire <95431075+akashiyaki01c@users.noreply.github.com>
2023-09-11 04:21:28 +0900

    svgファイルも公開

--- make.js
+++ make.js
@@ -1,5 +1,5 @@
-const startCharCode = 0x4E00;
-const endCharCode = 0x0A00;
+const startCharCode = 0x9000;
+const endCharCode = 0xA000;
 const charWidthPx = 24;
 const rowCount = 16;
 
@@ -8,20 +8,26 @@ svg.setAttribute("width", charWidthPx * rowCount);
 svg.setAttribute("height", charWidthPx * Math.floor((endCharCode-startCharCode) / rowCount))
 
 const g = document.createElementNS('http://www.w3.org/2000/svg','g');
+g.setAttribute("sodipodi:insensitive", "true");
 
 // 文字描画
-for (let i = startCharCode; i < endCharCode; i++) {
-    const char = document.createElementNS('http://www.w3.org/2000/svg','text');
-    char.setAttribute("x", i % rowCount * charWidthPx + charWidthPx/2);
-    char.setAttribute("y", Math.floor((i - startCharCode) / rowCount) * charWidthPx + charWidthPx/2 + 3);
-    char.setAttribute("font-size", 26);
-    char.setAttribute("text-anchor", "middle");
-    char.setAttribute("dominant-baseline", "middle");
-    char.setAttribute("font-family", "Noto Sans CJK JP");
-    char.setAttribute("style", "fill: red;")
-    char.innerHTML = String.fromCodePoint(i);
-    
-    g.appendChild(char);
+for (let i = startCharCode; i < endCharCode; i+= rowCount) {
+    const rowG = document.createElementNS('http://www.w3.org/2000/svg','g');
+    rowG.setAttribute("id", `row-${i.toString(16)}-${(i+rowCount-1).toString(16)}`)
+    for (let j = 0; j < rowCount; j++) {
+        const char = document.createElementNS('http://www.w3.org/2000/svg','text');
+        char.setAttribute("x", j * charWidthPx + charWidthPx/2);
+        char.setAttribute("y", (i-startCharCode)/rowCount * charWidthPx + charWidthPx/2 + 3);
+        char.setAttribute("font-size", 26);
+        char.setAttribute("text-anchor", "middle");
+        char.setAttribute("dominant-baseline", "middle");
+        char.setAttribute("font-family", "Noto Sans CJK JP");
+        char.setAttribute("style", "fill: red; line-height: 24px")
+        char.innerHTML = String.fromCodePoint(i+j);
+        
+        rowG.appendChild(char);
+    }
+    g.appendChild(rowG);
 }
 
 svg.appendChild(g);
